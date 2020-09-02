c9298a7cdeb9a8783bc81d66093ede53da4571d9
kazuki0606 <fkazuki666@icloud.com>
2020-09-02 16:19:22 +0900

    f

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.slider').slick({
+      dots: true, //下の丸ぽっち（インジケーター）を表示 
+      autoplay: true, //自動再生 
+      autoplaySpeed: 5000 //自動再生の速度 
+      //ここにオプションを書いていく 
+  });
+});
